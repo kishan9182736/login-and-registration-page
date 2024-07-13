0b9f61b60f8f4d47dee1e188d245c53f3d00b3b8
kishan <108215645+kishan9182736@users.noreply.github.com>
2024-07-13 09:08:48 +0600

    Add files via upload

---
+++ sample php/database.php
@@ -0,0 +1,14 @@
+<?php
+$hostName = "localhost";
+$dbUser = "root";
+$dbPassword = "root";
+$dbName = "login_register";
+
+// Create connection
+$conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
---
+++ sample php/login.php
@@ -0,0 +1,62 @@
+<?php
+session_start();
+if (isset($_SESSION["user"])) {
+    header("Location: index.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <?php
+          if (isset($_POST["login"])) {
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+            require_once "database.php"; 
+            
+            $stmt = $conn->prepare("SELECT * FROM users WHERE email = ?");
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $user = $result->fetch_assoc();
+            
+            if ($user) {
+                if (password_verify($password, $user["password"])) {
+                    $_SESSION["user"] = $user["id"]; // Use the user ID or any unique identifier
+
+                    header("Location: index.php");
+                    exit();
+                } else {
+                    echo "<div class='alert alert-danger'>Password does not match</div>";
+                }
+            } else {
+                echo "<div class='alert alert-danger'>Email does not match</div>";
+            }
+
+            $stmt->close();
+            $conn->close();
+        }
+        ?>
+        <form action="login.php" method="post">
+            <div class="form-group">
+                <input type="email" placeholder="Enter Email:" name="email" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <input type="password" placeholder="Enter Password:" name="password" class="form-control" required>
+            </div>
+            <div class="form-btn">
+                <input type="submit" value="Login" name="login" class="btn btn-primary">
+            </div>
+        </form>
+        <div><p>Not registered yet<a href="registration.php">Register Here</a></p></div>
+    </div>
+</body>
+</html>
